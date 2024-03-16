3503aba28b3d0f8796886769187d2ddceb7699b3
faisalhuston12 <116635508+Huston2023@users.noreply.github.com>
2024-03-16 10:54:54 +0200

    Add files via upload

---
+++ functions.dart
@@ -0,0 +1,19 @@
+void main() {
+  // Perform addition
+  int resultAddition = add(1, 3);
+  print("Result of addition: $resultAddition");
+
+  // Perform multiplication
+  int resultMultiplication = multiply(8, 4);
+  print("Result of multiplication: $resultMultiplication");
+}
+
+// Function to perform addition
+int add(int a, int b) {
+  return a + b;
+}
+
+// Function to perform multiplication
+int multiply(int a, int b) {
+  return a * b;
+}
---
+++ program1.dart
@@ -0,0 +1,8 @@
+void main() {
+  String name = "Faisal";
+  int age = 23;
+  String school = "Adventist University OF Central Africa";
+  String hobby = "playing basketball";
+  
+  print("My name is $name. I am $age years old, currently studying at $school, and my hobby is $hobby.");
+}
